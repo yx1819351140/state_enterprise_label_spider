77837b8f2ee910201134cb0c98363768e54bc73b
yangxin <1819351140@qq.com>
2024-09-04 15:04:01 +0800

    upt(国企标签):结构调整

---
+++ state_enterprise_label_spider/spiders/国企标签/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding:UTF-8 -*-
+# @Time    : 24.9.4 15:00
+# @Author  : yangxin
+# @Email   : yang1819351140@163.com
+# @IDE     : PyCharm
+# @File    : __init__.py.py
+# @Project : state_enterprise_label_spider
+# @Software: PyCharm
